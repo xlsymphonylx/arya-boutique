357e9f5ae4a2ea291ae5fc922403814da4c5dbe0
xlsymphonylx <xlsymphonylx@gmail.com>
2022-10-30 17:02:12 -0600

    removed unused getEffect

--- src/components/services/Grid.js
+++ src/components/services/Grid.js
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 import Item from "./Item";
 import styles from "../../styles/services/Grid.module.css";
 const services = require("../../assets/services.json");
